83732a5cdbe01151454a54e51c3689a4842d98c1
Krish Goyal <krishgoyal101@gmail.com>
2024-06-28 13:11:35 +0530

    28/6/24

--- components/Career/JobListings.tsx
+++ components/Career/JobListings.tsx
@@ -5,67 +5,109 @@ import React, { useState } from 'react';
 const JobListings = () => {
   const jobListings = [
     { 
-      title: 'Software Engineer', 
-      location: 'New York', 
+      title: 'Jr. Backend Engineer', 
+      location: 'Gurugram, Haryana (Work from office)', 
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus, enim ut euismod viverra, velit arcu viverra ipsum, non pulvinar arcu orci vel mi.',
-      languages: ['JavaScript', 'Python', 'Java'],
-      experience: '2+ years',
+      languages: ['JavaScript', 'Node.js', 'Express'],
+      experience: '1-2 years',
     },
     { 
-      title: 'UX/UI Designer', 
-      location: 'San Francisco', 
+      title: 'Sr. Backend Engineer', 
+      location: 'Gurugram, Haryana (Work from office)', 
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus, enim ut euismod viverra, velit arcu viverra ipsum, non pulvinar arcu orci vel mi.',
-      languages: ['HTML', 'CSS', 'JavaScript', 'React'],
-      experience: '3+ years',
-    },
-    { 
-      title: 'Data Analyst', 
-      location: 'Chicago', 
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus, enim ut euismod viverra, velit arcu viverra ipsum, non pulvinar arcu orci vel mi.',
-      languages: ['SQL', 'Python', 'R'],
-      experience: '2+ years',
-    },
-    { 
-      title: 'Product Manager', 
-      location: 'Boston', 
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus, enim ut euismod viverra, velit arcu viverra ipsum, non pulvinar arcu orci vel mi.',
-      languages: ['Agile', 'Scrum'],
+      languages: ['JavaScript', 'Python', 'MongoDB'],
       experience: '5+ years',
     },
     { 
+      title: 'Jr. Frontend Engineer', 
+      location: 'Gurugram, Haryana (Work from office)', 
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus, enim ut euismod viverra, velit arcu viverra ipsum, non pulvinar arcu orci vel mi.',
+      languages: ['HTML', 'CSS', 'JavaScript', 'React'],
+      experience: '1-2 years',
+    },
+    { 
+      title: 'Sr. Frontend Engineer', 
+      location: 'Gurugram, Haryana (Work from office)', 
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus, enim ut euismod viverra, velit arcu viverra ipsum, non pulvinar arcu orci vel mi.',
+      languages: ['React', 'TypeScript', 'Redux'],
+      experience: '5+ years',
+    },
+    { 
+      title: 'Lead Backend Engineer', 
+      location: 'Gurugram, Haryana (Work from office)', 
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus, enim ut euismod viverra, velit arcu viverra ipsum, non pulvinar arcu orci vel mi.',
+      languages: ['Java', 'Spring Boot', 'MySQL'],
+      experience: '8+ years',
+    },
+    { 
+      title: 'Lead Frontend Engineer', 
+      location: 'Gurugram, Haryana (Work from office)', 
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus, enim ut euismod viverra, velit arcu viverra ipsum, non pulvinar arcu orci vel mi.',
+      languages: ['Angular', 'Vue.js', 'JavaScript'],
+      experience: '8+ years',
+    },
+    { 
+      title: 'SDE Intern', 
+      location: 'Gurugram, Haryana (Work from office)', 
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus, enim ut euismod viverra, velit arcu viverra ipsum, non pulvinar arcu orci vel mi.',
+      languages: ['C++', 'Python'],
+      experience: 'Internship',
+    },
+    { 
+      title: 'Jr. SDET', 
+      location: 'Gurugram, Haryana (Work from office)', 
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus, enim ut euismod viverra, velit arcu viverra ipsum, non pulvinar arcu orci vel mi.',
+      languages: ['Python', 'Selenium', 'JUnit'],
+      experience: '1-2 years',
+    },
+    { 
+      title: 'Sr. SDET', 
+      location: 'Gurugram, Haryana (Work from office)', 
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus, enim ut euismod viverra, velit arcu viverra ipsum, non pulvinar arcu orci vel mi.',
+      languages: ['Java', 'TestNG', 'REST Assured'],
+      experience: '5+ years',
+    },
+    { 
+      title: 'Lead SDET', 
+      location: 'Gurugram, Haryana (Work from office)', 
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus, enim ut euismod viverra, velit arcu viverra ipsum, non pulvinar arcu orci vel mi.',
+      languages: ['C#', 'SpecFlow', 'Postman'],
+      experience: '8+ years',
+    },
+    { 
+      title: 'Junior QA (Manual)', 
+      location: 'Gurugram, Haryana (Work from office)', 
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus, enim ut euismod viverra, velit arcu viverra ipsum, non pulvinar arcu orci vel mi.',
+      languages: ['Manual Testing', 'Bug Tracking'],
+      experience: '1-2 years',
+    },
+    { 
+      title: 'Senior QA (Manual)', 
+      location: 'Gurugram, Haryana (Work from office)', 
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus, enim ut euismod viverra, velit arcu viverra ipsum, non pulvinar arcu orci vel mi.',
+      languages: ['Test Planning', 'Regression Testing'],
+      experience: '5+ years',
+    },
+    { 
+      title: 'Lead QA (Manual)', 
+      location: 'Gurugram, Haryana (Work from office)', 
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus, enim ut euismod viverra, velit arcu viverra ipsum, non pulvinar arcu orci vel mi.',
+      languages: ['Quality Assurance', 'Defect Management'],
+      experience: '8+ years',
+    },
+    { 
       title: 'DevOps Engineer', 
-      location: 'Seattle', 
+      location: 'Gurugram, Haryana (Work from office)', 
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus, enim ut euismod viverra, velit arcu viverra ipsum, non pulvinar arcu orci vel mi.',
-      languages: ['AWS', 'Docker', 'Kubernetes'],
-      experience: '4+ years',
-    },
-    { 
-      title: 'Marketing Specialist', 
-      location: 'Austin', 
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus, enim ut euismod viverra, velit arcu viverra ipsum, non pulvinar arcu orci vel mi.',
-      languages: ['SEO', 'Content Marketing'],
+      languages: ['Docker', 'Kubernetes', 'CI/CD'],
       experience: '3+ years',
     },
     { 
-      title: 'Sales Representative', 
-      location: 'Miami', 
+      title: 'BI Developer', 
+      location: 'Gurugram, Haryana (Work from office)', 
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus, enim ut euismod viverra, velit arcu viverra ipsum, non pulvinar arcu orci vel mi.',
-      languages: ['Negotiation', 'CRM'],
-      experience: '2+ years',
-    },
-    { 
-      title: 'Network Engineer', 
-      location: 'Denver', 
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus, enim ut euismod viverra, velit arcu viverra ipsum, non pulvinar arcu orci vel mi.',
-      languages: ['Cisco', 'Juniper'],
+      languages: ['SQL', 'Tableau', 'Power BI'],
       experience: '3+ years',
-    },
-    { 
-      title: 'Technical Writer', 
-      location: 'Los Angeles', 
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus, enim ut euismod viverra, velit arcu viverra ipsum, non pulvinar arcu orci vel mi.',
-      languages: ['English', 'Technical Writing'],
-      experience: '2+ years',
     },
   ];
 
@@ -79,7 +121,7 @@ const JobListings = () => {
     <div className="container mx-auto mt-8 mb-16 bg-white">
       <h2 className="text-4xl font-semibold mb-8 text-center">Job Listings</h2>
       {jobListings.map((job, index) => (
-        <div key={index} className="mb-6 rounded-lg overflow-hidden shadow-md border border-gray-300">
+        <div key={index} className="mb-4 rounded-md overflow-hidden shadow-lg border border-gray-300">
           <div
             className="p-4 flex justify-between items-center cursor-pointer bg-blue-100"
             onClick={() => toggleExpand(index)}
@@ -107,6 +149,11 @@ const JobListings = () => {
           )}
         </div>
       ))}
+      <div className="text-center text-gray-600 mt-4 font-bold">
+        <p>If you are unable to submit the form or have more queries, feel free to reach out to us at</p>
+           <Link href="mailto:admin@digicraft.com"><p className="text-blue-500 hover:underline">admin@digicraft.com</p></Link>
+            <p>or call us at <Link href="tel:+911234567890" className="text-blue-500 hover:underline">+91 1234567890</Link>. We are here to help you with any questions or concerns you may have.</p>
+      </div>
     </div>
   );
 };
